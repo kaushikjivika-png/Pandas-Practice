08a069da3bfdd2ac8accde184096108772f087e1
Jivika Kaushik <kaushikjivika@gmail.com>
2026-08-25 12:01:41 +0530

    Add introduction to Pandas library
    Added introductory content about Pandas library, including its purpose, unique features, and main data structures.

---
+++ 01_Basics/01_introduction_to_pandas.py
@@ -0,0 +1,25 @@
+# Introduction to Pandas
+
+# Pandas is a Python library used for data manipulation (cleaning,  transforming, and structuring data)
+# and data analysis (finding patterns, insights and trends).
+
+# What makes pandas unique?
+
+# It works seamlesly with structured data formats like csv, json and excel.
+
+# Pandas provides two main data structures:
+
+# 1. Series - It is a one-dimensional labelled array that can hold any data type:
+#             int, float, str etc.
+
+# 2. DataFrame - It is a two dimensional labelled array data structure, similar to a table in excel or Sql table.
+
+# Example:
+# A simple table-like dataset
+
+students = {
+    "Name": ["Jivika", "Rahul", "Priya"],
+    "Age": [20, 21, 19]
+}
+
+print(students)
