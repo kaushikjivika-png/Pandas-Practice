7c9b69760cdb1963f486db3839129db582190f47
Jivika Kaushik <kaushikjivika@gmail.com>
2026-08-26 11:49:58 +0530

    Add installation and import instructions for Pandas

---
+++ 01_Basics/02_Installing_and_importing_Pandas.py
@@ -0,0 +1,13 @@
+# Installing and Importing Pandas
+
+# To install Pandas, use the following command in the terminal:
+# pip install pandas
+
+# After installing Pandas, we can import it into our Python program.
+
+import pandas as pd
+
+# "pd" is the commonly used alias for Pandas.
+
+# Checking the Pandas version
+print(pd.__version__)
